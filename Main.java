373e9fb787df6b2060c2d4f1420a039559f02fbc
RezkySaputraa <145405371+RezkySaputraa@users.noreply.github.com>
2023-09-22 23:30:44 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package pkg7_eksplorevariable;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        String nama_depan, nama_belakang, nama_lengkap; 
+        
+        nama_depan = "Rezky";
+        nama_belakang = "aulia";
+        
+        System.out.print("Nama depan lo \t\t: ");
+        System.out.println(nama_depan);
+        
+        System.out.print("Nama Belakang lo \t: ");
+        System.out.println(nama_belakang);
+        
+        nama_lengkap = (nama_depan + " " + nama_belakang);
+        System.out.print("Nama lengkap lo \t: ");
+        System.out.println(nama_lengkap);
+                
+    }
+    
+}
